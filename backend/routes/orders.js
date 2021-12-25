85140add4ffb03cb941b37174b508e7d88221714
Matthew Kiggen <28730291+mattkiggen@users.noreply.github.com>
2021-12-25 13:05:25 +0200

    add order creation

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
+const auth = require('../middleware/auth');
+const asyncMiddleware = require('../middleware/asyncMiddleware');
+const prisma = require('../lib/prisma');
 
 router.get('/', async (req, res) => {
   res.send('all orders for a user');
 });
 
 // Create new order
-router.post('/', async (req, res) => {
-  res.send('New order created...');
-});
+router.post(
+  '/',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const order = await prisma.order.create({
+      data: {
+        userId: req.user.id,
+        orderDetails: {
+          create: req.body,
+        },
+      },
+    });
+
+    res.send(order);
+  })
+);
 
 module.exports = router;
