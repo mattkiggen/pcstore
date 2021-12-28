3f8267844b02aca75cfedbf144c149e63538f244
Matthew Kiggen <28730291+mattkiggen@users.noreply.github.com>
2021-12-28 10:26:01 -0800

    add comment

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,5 +1,6 @@
 function error(err, req, res, next) {
   // TODO: refactor this so we can provide detailed errors
+  // can create an error object and pass it here, then set a dynamic status
   console.log(err);
   res.status(500).send('Something broke!');
 }
