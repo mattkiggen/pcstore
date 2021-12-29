a0546f5d1770e812721775ecf826077b940940c7
Matthew Kiggen <28730291+mattkiggen@users.noreply.github.com>
2021-12-29 19:24:04 +0200

    Add successful login toast

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -2,6 +2,8 @@ import Navbar from '../components/Navbar';
 import { useState } from 'react';
 const axios = require('axios').default;
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+import toast from 'react-hot-toast';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
@@ -10,44 +12,58 @@ export default function LoginPage() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await axios.post(`${process.env.API_URL}/api/auth`, {
-      email,
-      password,
-    });
+    try {
+      const res = await axios.post(`${process.env.API_URL}/api/auth`, {
+        email,
+        password,
+      });
 
-    if (res.status === 200) {
-      localStorage.setItem('x-auth-token', res.data.token);
-      return router.push('/dashboard');
+      if (res.status === 200) {
+        localStorage.setItem('x-auth-token', res.data.token);
+        toast.success('Login was successful');
+        setTimeout(() => {
+          return router.push('/dashboard');
+        }, 2000);
+      }
+    } catch (err) {
+      toast.error('An error occured. Please try again');
     }
-    console.log(res.status);
   };
 
   return (
     <>
       <Navbar />
       <div className='container mx-auto p-6'>
-        <h1>Login</h1>
+        <h1 className='text-3xl mb-6'>Login to Your Account</h1>
         <form onSubmit={handleSubmit}>
-          <label className='flex flex-col'>
+          <label className='flex flex-col mb-6'>
             <span>Email:</span>
             <input
               type='text'
-              className='p-2 rounded shadow'
+              className='p-2 rounded border border-gray-200'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
           </label>
-          <label className='flex flex-col'>
+          <label className='flex flex-col mb-8'>
             <span>Password:</span>
             <input
               type='password'
-              className='p-2 rounded shadow'
+              className='p-2 rounded border border-gray-200'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </label>
-          <button className='px-6 py-3 bg-gray-400'>Login</button>
+          <button className='px-6 py-2 bg-gray-800 rounded text-white mb-6'>
+            Login
+          </button>
         </form>
+        <p>
+          Don't have an account?{' '}
+          <Link href='/register'>
+            <a className='underline'>Register</a>
+          </Link>
+        </p>
       </div>
     </>
   );
